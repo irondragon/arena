341ce8be1a4a4232ff50c3bb793d5931f050cc02
irondragon <bulat.d.idiatullin@gmail.com>
2015-01-23 22:48:30 +0300

    commit properties of core

--- src/com/moscow/java/Core.java
+++ src/com/moscow/java/Core.java
@@ -6,4 +6,17 @@ package com.moscow.java;
 
 public class Core {
 
+    private String name;
+
+    public Core(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
